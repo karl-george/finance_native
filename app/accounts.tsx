b0a83a2cf77881b0f7b11ac67977ac8d6e03370e
Karl George <karlgeorge87@gmail.com>
2024-05-06 17:59:36 +0200

    Adding new account styling

--- app/accounts.tsx
+++ app/accounts.tsx
@@ -1,7 +1,15 @@
-import { Text, View } from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
 import AccountsList from '../components/AccountsList';
+import { useState } from 'react';
 
 const Accounts = () => {
+  const [newAccountName, setNewAccountName] = useState('');
+
+  const createAccount = () => {
+    console.warn('create account');
+  };
+
   return (
     <View>
       <View className='flex-row justify-between px-4 py-2'>
@@ -10,6 +18,12 @@ const Accounts = () => {
         <Text>TAP</Text>
       </View>
       <AccountsList />
+      <View className='flex-row justify-between px-4 py-2 mt-2 bg-white'>
+        <TextInput placeholder='Name'></TextInput>
+        <TextInput placeholder='CAP %'></TextInput>
+        <TextInput placeholder='TAP %'></TextInput>
+      </View>
+      <Button title='Add account' onPress={createAccount} />
     </View>
   );
 };
