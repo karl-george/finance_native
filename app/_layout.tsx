2ab13bc334243ecf25c12fa7e0b38f09199a5d49
Karl George <karlgeorge87@gmail.com>
2024-05-06 17:04:04 +0200

    Add bottom tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,33 @@
 import { Tabs } from 'expo-router';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const RootLayout = () => {
-  return <Tabs />;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: 'Allocations',
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name='account-tree' size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='accounts'
+        options={{
+          title: 'Accounts',
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name='account-balance-wallet'
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 };
 
 export default RootLayout;
